d9e5991499e64bab4c3694ae837b035394c7c3c4
karynntran <karynn.tran@gmail.com>
2020-01-16 15:14:39 -0500

    Initial Commit

---
+++ itineshary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itineshary
+//
+//  Created by Karynn Elio on 1/16/20.
+//  Copyright © 2020 karynntran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ itinesharyTests/itinesharyTests.swift
@@ -0,0 +1,34 @@
+//
+//  itinesharyTests.swift
+//  itinesharyTests
+//
+//  Created by Karynn Elio on 1/16/20.
+//  Copyright © 2020 karynntran. All rights reserved.
+//
+
+import XCTest
+@testable import itineshary
+
+class itinesharyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
